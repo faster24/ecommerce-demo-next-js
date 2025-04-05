c7afa684f3c3c8c68185190e8c80a2178b4baf6e
Nyi Ye Naing <45783468+faster24@users.noreply.github.com>
2025-04-06 02:28:11 +0700

    fix: checkout error

--- pages/checkout/payment-info.js
+++ pages/checkout/payment-info.js
@@ -79,7 +79,19 @@ function PaymentInfo() {
                     <div className="col">
                       <label className="form-label">Expiry</label>
                       <select className="form-select">
-                        <option>Month</option>
+                        <option value="">Month</option>
+                        <option value="01">January</option>
+                        <option value="02">February</option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
                       </select>
                     </div>
                     <div className="col mt-auto">
